293c416c2af26fa1c905ff856435541551f89b5d
Ubuntu <ubuntu@localhost.localdomain>
2025-04-21 21:26:44 +0800

    change prameters

--- scenerf/scripts/single_image_infer.py
+++ scenerf/scripts/single_image_infer.py
@@ -27,9 +27,9 @@ def disparity_normalization_vis(disparity):
 
 def main():
     # 设置参数
-    model_path = "C:/Users/Bruce Wayne/Downloads/scenerf_kitti.ckpt"  # 模型路径
-    img_path = "C:/Users/Bruce Wayne/Downloads/building.jpg"     # 输入图像路径
-    save_dir = "D:/workspace/SceneRF-main/results/reconstruction/colors"          # 保存结果路径
+    model_path = "/home/ubuntu/scenerf_bundlefusion.ckpt"  # 模型路径
+    img_path = "/home/ubuntu/monoSceneRF-infer/scenerf/scripts/building.jpg"     # 输入图像路径
+    save_dir = "/home/ubuntu/monoSceneRF-infer/results/reconstruction/colors"          # 保存结果路径
     
     # 清理显存
     clear_gpu_memory()
